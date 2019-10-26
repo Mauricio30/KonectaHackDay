70b0464d3f7a7381c6f56606fbeb453497121947
gerardoramos89 <coordtss2@gmail.com>
2019-10-26 12:48:28 -0500

    task10

--- WebApi/src/components/login.js
+++ WebApi/src/components/login.js
@@ -4,40 +4,68 @@ import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
-function App() {
-  return (
-    <div className="App">
+class login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={
+        username:'',
+        isLogin:true,
+        loginmessage:'',
+        password:''
+        }
+       }
 
-      <div>
-        <MuiThemeProvider>
-          <div>
-          <AppBar
-             title="Login"
-           />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             //onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               //onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-             <br/>
-             <RaisedButton label="Register" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+       handleClick(event){
+         console.log("event",this.state);
 
- </div>
-         </MuiThemeProvider>
-      </div>
-    </div>
-  );
-}
-const style = {
-    margin: 15,
-   };
-export default App;
+         var loginmessage;
+      var loginscreen=[];
+      loginmessage = "Already registered.Go to Login";
+      this.setState({
+                     loginscreen:loginscreen,
+                     loginmessage:loginmessage,
+                     buttonLabel:"Login",
+                     isLogin:false
+                   })
+   
+        
+      }
+
+      render() {
+          return (
+            <div>
+              <MuiThemeProvider>
+                <div>
+                <AppBar
+                   title="Login"
+                 />
+                 <TextField
+                   hintText="Enter your Username"
+                   floatingLabelText="Username"
+                   onChange = {(event,newValue) => this.setState({username:newValue})}
+                   />
+                 <br/>
+                   <TextField
+                     type="password"
+                     hintText="Enter your Password"
+                     floatingLabelText="Password"
+                     onChange = {(event,newValue) => this.setState({password:newValue})}
+                     />
+                   <br/>
+                   <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                   <br/>
+                   <RaisedButton label="Register" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                   <br/>
+                   {this.state.loginmessage}
+               </div>
+               </MuiThemeProvider>
+            </div>
+          );
+        }
+      }
+      const style = {
+       margin: 15,
+      };
+export default login;
+
+
