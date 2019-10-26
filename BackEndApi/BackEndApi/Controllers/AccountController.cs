6c1fc14586ec0b76cc407c32e7291fe75f5daad7
Jair Piedrahita Vargas <jairp8@gmail.com>
2019-10-26 11:48:42 -0500

    Cambios iniciales app

---
+++ BackEndApi/BackEndApi/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BackEndApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BackEndApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        [HttpPost("Authenticate")]
+        [AllowAnonymous]
+        public IActionResult Authenticate([FromBody] AuthenticationDTO authenticationDTO)
+        {
+            var claims = new List<Claim>
+                    {
+                        new Claim("roles", "vendedor"),
+                        new Claim(JwtRegisteredClaimNames.Sub, authenticationDTO.UserName),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Email, authenticationDTO.UserName)
+                    };
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(
+                System.Text.Encoding.UTF8.GetBytes("TokenKey")
+                );
+            var signingCredentials = new SigningCredentials(
+                symmetricSecurityKey, SecurityAlgorithms.HmacSha256
+                );
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: "ThunderHacks",
+                audience: "Konecta",
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(20),
+                signingCredentials: signingCredentials
+                );
+
+            var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(token);
+        }
+
+    }
+}
---
+++ BackEndApi/BackEndApi/DAL/AuthenticationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BackEndApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndApi.DAL
+{
+    public class AuthenticationDbContext : DbContext
+    {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+    }
+}
