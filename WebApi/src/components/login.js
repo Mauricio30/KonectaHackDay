f201674a50ad75a3ca8d3e90206a864ffb6ceffd
gerardoramos89 <coordtss2@gmail.com>
2019-10-26 12:22:32 -0500

    task7

---
+++ WebApi/src/components/login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             //onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               //onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+             <br/>
+             <RaisedButton label="Register" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+
+ </div>
+         </MuiThemeProvider>
+      </div>
+    </div>
+  );
+}
+const style = {
+    margin: 15,
+   };
+export default App;
